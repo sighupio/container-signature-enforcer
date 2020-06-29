8c4f5a814bf5c538cd50a43a7c9abc3060e17e94
Philippe Scorsolini <pscorso93@gmail.com>
2020-06-29 18:23:15 +0200

    1 more testcase for reference

--- notary/reference_test.go
+++ notary/reference_test.go
@@ -11,6 +11,7 @@ func TestReferenceOK(t *testing.T) {
 		{image: "docker.io/test/alpine:test", original: "docker.io/test/alpine:test", name: "docker.io/test/alpine", tag: "test", digest: "", hostname: "docker.io", port: ""},
 		{image: "registry.hub.docker.com/library/alpine:test", original: "registry.hub.docker.com/library/alpine:test", name: "registry.hub.docker.com/library/alpine", tag: "test", digest: "", hostname: "registry.hub.docker.com", port: ""},
 		{image: "alpine", original: "alpine", name: "docker.io/library/alpine", tag: "latest", digest: "", hostname: "docker.io", port: ""},
+		{image: "alpine@sha256:2bb501e6173d9d006e56de5bce2720eb06396803300fe1687b58a7ff32bf4c14", original: "alpine@sha256:2bb501e6173d9d006e56de5bce2720eb06396803300fe1687b58a7ff32bf4c14", name: "docker.io/library/alpine", tag: "latest", digest: "2bb501e6173d9d006e56de5bce2720eb06396803300fe1687b58a7ff32bf4c14", hostname: "docker.io", port: ""},
 	}
 	for _, tt := range tests {
 		t.Run(tt.image, func(t *testing.T) {
