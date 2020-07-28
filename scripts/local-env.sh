f1d854c040cd4afc19ca79f6deb2eee59b4ff610
Ángel Barrera Sánchez <angel@sighup.io>
2020-07-28 18:28:16 +0200

    Working all server-side on linux

--- scripts/local-env.sh
+++ scripts/local-env.sh
@@ -58,54 +58,32 @@ retry 10 kubectl apply --validate=false -f https://github.com/jetstack/cert-mana
 helm upgrade --install cert-manager jetstack/cert-manager --namespace cert-manager --version v0.15.2
 kubectl wait --for=condition=Available deployment --timeout=3m -n cert-manager --all
 
+echo "    Creating PKI"
+retry 10 kubectl apply -f scripts/notary.pki.yaml
+kubectl wait --for=condition=Ready certs --timeout=3m -n notary --all
+
 echo "2. Deploying auth service"
 retry 10 kubectl apply -f scripts/mock-auth-server.yaml
-kubectl wait --for=condition=Ready certs --timeout=3m -n notary --all
 kubectl wait --for=condition=Available deployment --timeout=3m -n notary --all
 
 echo "3. Deploying docker registry"
-retry 10 kubectl apply -f scripts/docker-registry-pki.yaml
-kubectl wait --for=condition=Ready certs --timeout=3m -n notary --all
 helm upgrade --install registry stable/docker-registry --values scripts/docker-registry-values.yaml -n notary --version 1.9.4
 kubectl wait --for=condition=Available deployment --timeout=3m -n notary --all
 
 echo "4. Deploying notary"
-retry 10 kubectl apply -f scripts/notary-pki.yaml
-kubectl wait --for=condition=Ready certs --timeout=3m -n notary --all
 kubectl apply -f scripts/notary.yaml
 kubectl wait --for=condition=Available deployment --timeout=3m -n notary --all
 
 echo "4. Copying notary-server certificates to webhook namespace"
 kubectl get secret notary-server-crt -n notary -o yaml | sed s@"namespace: notary"@"namespace: webhook"@ | kubectl apply -n webhook -f -
 
-echo "5. Generating delegation key"
-cat <<EOF | kubectl apply -f -
-apiVersion: cert-manager.io/v1alpha2
-kind: Issuer
-metadata:
-  name: selfsigned-issuer
-spec:
-  selfSigned: {}
-EOF
-cat <<EOF | kubectl apply -f -
-apiVersion: cert-manager.io/v1alpha2
-kind: Certificate
-metadata:
-  name: delegation-key
-spec:
-  secretName: delegation-key
-  dnsNames:
-    - delegation-key
-  issuerRef:
-    name: selfsigned-issuer
-EOF
-kubectl wait --for=condition=Ready certs --timeout=3m --all
-kubectl get secret delegation-key -o jsonpath='{.data.tls\.crt}' | base64 -d > delegation.crt
-kubectl get secret delegation-key -o jsonpath='{.data.tls\.key}' | base64 -d > delegation.key
+echo "5. Downloading delegation key"
+kubectl get secret -n notary delegation-key -o jsonpath='{.data.tls\.crt}' | base64 -d > delegation.crt
+kubectl get secret -n notary delegation-key -o jsonpath='{.data.tls\.key}' | base64 -d > delegation.key
 chmod 744 delegation.crt
 chmod 700 delegation.key
-echo "  Delegation key available at ./delegation.crt"
+echo "    Delegation key available at ./delegation.crt"
 
-echo "6. Downloading notary server certificate"
-kubectl get secret -n notary notary-server-crt -o jsonpath='{.data.tls\.crt}' | base64 -d > notary-tls.crt
-echo "  Notary Server certificate available at ./notary-tls.crt"
+echo "6. Downloading ca certificate"
+kubectl get secret -n notary root-ca -o jsonpath='{.data.tls\.crt}' | base64 -d > ca.crt
+echo "    CA certificate available at ./ca.crt"
