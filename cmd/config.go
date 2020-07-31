15bc181134de27901b0ff2b463b54afe3b3cd4b2
Philippe Scorsolini <pscorso93@gmail.com>
2020-07-31 17:48:49 +0200

    using secret instead of configmap for repositories

--- cmd/config.go
+++ cmd/config.go
@@ -10,17 +10,17 @@ import (
 var globalConfig = conf.NewGlobalConfig()
 
 func reloadConfig(e fsnotify.Event) {
-	logrus.WithField("file", globalConfig.ConfigPath).Info("Config file modified.")
+	reloadLogger := logrus.WithField("phase", "reloadConfig")
+	reloadLogger.WithField("file", globalConfig.ConfigPath).Info("Config file modified.")
 	newConfig := conf.Config{}
 	if err := viper.Unmarshal(&newConfig); err != nil {
 		logrus.WithError(err).Error("error unmarshalling new config")
 		return
 	}
-	reloadLogger := logrus.WithField("phase", "reloadConfig")
 	if err := newConfig.Validate(reloadLogger); err != nil {
 		reloadLogger.WithError(err).Error("Error validating new config reloaded, fallbacking to previous one.")
 		return
 	}
 	globalConfig.SetConfig(&newConfig)
-	logrus.WithField("config", globalConfig.GetConfig()).Printf("New config loaded")
+	reloadLogger.WithField("config", globalConfig.GetConfig()).Printf("New config loaded")
 }
